03dc977481390bacbb3ff569c8ac482d7b666065
Scott Onson <onsons@ssoworld.com>
2020-05-15 11:10:05 -0500

    adding feedback echos to shell

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 #insert pre-build script here.
-
+echo -n "executing pre-build steps"
 
 
 #build steps for cmake in unix
+echo -n "creating CMAKE project and building source"
 mkdir build
 cmake -S . -B build
 cmake --build build
 
 #insert post-build script here
+echo -n "executing post-build steps"
