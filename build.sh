f2892db5aecbe7f38eb4587e76ef55aa631a6de8
Scott Onson <onsons@ssoworld.com>
2020-05-15 13:53:10 -0500

    linux version test

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ echo -n "executing pre-build steps"
 #build steps for cmake in unix
 echo -n "creating CMAKE project and building source"
 mkdir build
-cmake -S . -B build
+cd build
+cmake -S .. -B build
 cmake --build build
 
 #insert post-build script here
